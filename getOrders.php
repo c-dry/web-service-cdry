df4044f03776e23f99abc958c96533371015eaa7
diasantoso <mikhaelmds@gmail.com>
2017-04-27 16:47:00 +0700

    Benerin lagi

--- getOrders.php
+++ getOrders.php
@@ -3,28 +3,29 @@ define('HOST','mysql.idhostinger.com');
 define('USER','u917918641_cdry');
 define('PASS','123456');
 define('DB','u917918641_cdry');
- 
+
   $con = mysqli_connect(HOST,USER,PASS,DB);
   $em  = $_GET['email'];
   $sql = "select * from orders where status='Undone' and email='".$em."'";
 
   $res = mysqli_query($con,$sql);
-  
+
   $result = array();
-  
+
   while($row = mysqli_fetch_array($res)){
     array_push($result,
     array('id_order'=>$row[0],
-	'email'=>$row[1],
-    'weight'=>$row[2],
-	'price'=>$row[3],
-	'date_order'=>$row[4],
-	'date_end'=>$row[5],
-    'status'=>$row[6]
+  'email'=>$row[1],
+  'address'=>$row[2],
+    'weight'=>$row[3],
+	'price'=>$row[4],
+	'date_order'=>$row[5],
+	'date_end'=>$row[6],
+    'status'=>$row[7]
   ));
 }
  echo json_encode(array("result"=>$result));
- 
+
   mysqli_close($con);
- 
+
 ?>
